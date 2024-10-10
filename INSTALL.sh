bc2279ef2e43ad04f0c5d45101ad01fcc56aa45f
gopuman <gopuman@node0.gopuman-222524.resilientdb-pg0.utah.cloudlab.us>
2024-10-10 15:14:18 -0600

    Updated INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -17,18 +17,27 @@ echo "Installing ResilientDB..."
 git clone https://github.com/apache/incubator-resilientdb.git /opt/incubator-resilientdb
 cd /opt/incubator-resilientdb
 ./INSTALL.sh
+./service/tools/kv/server_tools/start_kv_service.sh
+bazel build service/tools/kv/api_tools/kv_service_tools
 
 # Install PythonSDK
 echo "Installing PythonSDK..."
 git clone https://github.com/apache/incubator-resilientdb-python-sdk.git /opt/incubator-resilientdb-python-sdk
 cd /opt/incubator-resilientdb-python-sdk
-./INSTALL.sh
+sh ./INSTALL.sh
+python3.10 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+bazel build service/http_server/crow_service_main
+bazel-bin/service/http_server/crow_service_main service/tools/config/interface/client.config service/http_server/server_config.config
+deactivate
 
 # Install ResDBORM
 echo "Installing ResDBORM..."
 git clone https://github.com/ResilientEcosystem/ResDB-ORM.git /opt/ResDB-ORM
 cd /opt/ResDB-ORM
 ./INSTALL.sh
+pip install resdb-orm
 
 # Install Smart-Contracts CLI
 echo "Installing Smart-Contracts CLI..."
@@ -38,7 +47,7 @@ npm install -g rescontract-cli
 echo "Installing Smart-Contracts GraphQL..."
 git clone https://github.com/ResilientEcosystem/smart-contracts-graphql.git /opt/smart-contracts-graphql
 cd /opt/smart-contracts-graphql
-./INSTALL.sh
+npm install
 
 # Install ResVault
 echo "Installing ResVault..."
