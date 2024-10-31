50daf741b6a93bb5de4d10b49419238982108fe8
gopuman <gopuman@node0.gopuman-226961.resilientdb-pg0.utah.cloudlab.us>
2024-10-31 16:44:35 -0600

    Add Changelog and Contributors workflow

---
+++ scripts/update_contributors.py
@@ -0,0 +1,25 @@
+import requests
+import os
+
+query = """
+{
+  repository(owner:"ResilientEcosystem", name:"resilientdb-quickstart") {
+    collaborators(first: 100) {
+      edges {
+        node {
+          login
+        }
+      }
+    }
+  }
+}
+"""
+
+headers = {"Authorization": f"Bearer {os.getenv('GITHUB_TOKEN')}"}
+response = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+contributors = response.json()['data']['repository']['collaborators']['edges']
+
+with open('docs/contributors.md', 'w') as file:
+    file.write("# Contributors\n\n")
+    for contributor in contributors:
+        file.write(f"- {contributor['node']['login']}\n")
